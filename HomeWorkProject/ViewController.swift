89d094ed5470aa1d6d16f182313d05424df3dc9a
DwightShrut <skatemax@mail.ru>
2025-06-14 20:53:35 +0300

    Initial Commit

---
+++ HomeWorkProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWorkProject
+//
+//  Created by Макс Макеев on 14.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
